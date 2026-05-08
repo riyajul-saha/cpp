e14a625103f47d3ae68dfa543d2c384efdeba9a6
riyajul-saha <riyajsaha879@hotmail.com>
2026-05-08 15:43:01 +0000

    solve automophoric_number.cpp

--- automophoric_number.cpp
+++ automophoric_number.cpp
@@ -1,9 +1,59 @@
 #include<iostream>
+// #include<math>
 using namespace std;
+
+int length(int num)
+{
+  int len=0;
+  while(num > 0)
+  {
+    len++;
+    num = num/10;
+  }
+  return len;
+}
+
+int calculate_exponant(int num,int power)
+{
+  int result=1;
+  for(int i = 0; i < power; i++)
+  {
+    result=result*num;
+  }
+  return result;
+}
+
+bool is_automorphic(int num)
+{
+  int sqare = calculate_exponant(num,2);
+  int len = length(num);
+  int devisor = calculate_exponant(10,len);
+  int result = (sqare - num)%devisor;
+  if(result == 0)
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
+
 
 int main()
 {
-  cout<<"Automophoric Number: Test1 Sucessfull"<<endl;
-  cout<<"Automophoric Number: Test2k Sucessfull"<<endl;
+  int number;
+  int len;
+  cout<<"Enter a Number :";
+  cin>>number;
+  if(is_automorphic(number))
+  {
+    cout<<number<<" is a automophoric number"<<endl;
+  }
+  else
+  {
+    cout<<number<< " is ~not a automophoric number" <<endl;
+  }
+  
   return 0;
 }
